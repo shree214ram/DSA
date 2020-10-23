9e27f862ea4e587f2a3052da0abf2f2e80fb69b2
sunny_parmar_300041371 <sunny.parmar@myntra.com>
2020-10-23 08:49:20 +0530

    second

--- Data Structure/String/GivenAStringFindItsFirstNonRepeatingCharacter/GivenAStringFindItsFirstNonRepeatingCharacter.js
+++ Data Structure/String/GivenAStringFindItsFirstNonRepeatingCharacter/GivenAStringFindItsFirstNonRepeatingCharacter.js
@@ -13,8 +13,9 @@ class HashTable {
         if (!this.bucket[index]) {
             this.bucket[index] = {}
             this.size++
+            this.keys.push(key)
         }
-        this.bucket[index] = { key: value }
+        this.bucket[index] = { key: key, value: value }
     }
     isEmpty() {
         return this.size == 0
@@ -26,10 +27,13 @@ class HashTable {
         }
         return false;
     }
+    containsKey(key){
+        return this.keys.includes(key)
+    }
     size() {
         return this.size
     }
-    bucket() {
+    bucketResult() {
         return this.bucket
     }
     hashFunc(key) {
@@ -39,19 +43,35 @@ class HashTable {
 
 
 const str = "geeksforgeeks";
+console.log('Input String is => ',str)
+
 const hash = new HashTable();
 
+const k = 97;
+  const charObject = {};
+  for (let i = 0; i < 26; i++) {
+    const currentObject = String.fromCharCode(k + i);
+    charObject[currentObject] = k + i;
+  }
+
 for (let i = 0; i < str.length; i++) {
-    if (hash.get(str[i].charAt)) {
-        const value = hash.get(str[i].charAt).value + 1;
-        hash.set(str[i].charAt, value)
+    const currentKey= charObject[str[i]]
+    if (hash.containsKey(currentKey)) {
+        const value = hash.get(currentKey).value + 1;
+        hash.set(currentKey, value)
     } else {
-        hash.set(str[i].charAt, 1)
+        hash.set(currentKey, 1)
     }
 }
-
-for (let i = 0; i < hash.bucket.length; i++) {
-    if (hash.bucket[i].value==1) {
-        console.log("First Non repeating character is ", hash.bucket[i].key)
+const hashBucket = hash.bucketResult();
+for (let i = 0; i < hashBucket.length; i++) {
+    if (hashBucket[i] && hashBucket[i].value==1) {
+        for (var prop in charObject) {
+            if (charObject[prop] == hashBucket[i].key) {
+                // your code
+                console.log("First Non repeating character is ", prop);
+                break;
+            }
+        }
     } 
 }
