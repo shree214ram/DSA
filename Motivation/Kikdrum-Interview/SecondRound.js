5afafb0e48ee6a470101260cc75882abc9955530
shree214ram <shree214ram>
2022-12-23 14:15:47 +0530

    Practiced DSA

--- Motivation/Kikdrum-Interview/SecondRound.js
+++ Motivation/Kikdrum-Interview/SecondRound.js
@@ -5,3 +5,8 @@ Implement React and Redux
 4. controlled over safe rerendering . 
 
 https://codesandbox.io/s/react-redux-demo-sunny-react-redux-api-data-wh8vf1?file=/src/FunctionBased.js
+
+1. UI should be more Component wise 
+2. Handle UseIffect properly and understand deep dive UseMemo and useCallBack , react Memo
+3. Controlling over componanant rendering and stop unnecessory rendering 
+4. 
